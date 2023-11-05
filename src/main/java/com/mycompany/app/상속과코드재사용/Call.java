8fa85208d13f6526f6ff36c65a168f04dce6d09c
Dmori <83327857+Dimo-2562@users.noreply.github.com>
2023-11-05 01:46:31 +0000

    Typing Chater10 codes partitions

---
+++ src/main/java/com/mycompany/app/상속과코드재사용/Call.java
@@ -0,0 +1,25 @@
+package com.mycompany.app.상속과코드재사용;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+/*
+ * 개별 통화 기간을 저장하는 클래스
+ */
+public class Call {
+    private LocalDateTime from; //통화 시작시간
+    private LocalDateTime to; //통화 종료시간
+
+    public Call(LocalDateTime from, LocalDateTime to){
+        this.from = from;
+        this.to = to;
+    }
+
+    public Duration getDuration(){
+        return Duration.between(from, to);
+    }
+
+    public LocalDateTime getFrom(){
+        return from;
+    }
+}
---
+++ src/main/java/com/mycompany/app/상속과코드재사용/Phone.java
@@ -0,0 +1,5 @@
+package com.mycompany.app.상속과코드재사용;
+
+public class Phone {
+    
+}
